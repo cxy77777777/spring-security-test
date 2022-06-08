8ae8bfcde5a1547b94f5206f076e8e60c3d9b10f
fuyunhao <fh6584207>
2022-06-08 19:06:17 +0800

    添加 鉴权注释

--- src/main/java/com/sangeng/sangengspringsecurity/controller/HelloController.java
+++ src/main/java/com/sangeng/sangengspringsecurity/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("getHello")
-    @PreAuthorize("hasAuthority('test')")
+    @PreAuthorize("hasAuthority('test')")//具有test权限才可以访问，该注解编译时会把内部字符串当作函数执行，hasAuthority('test')，返回布尔类型，true允许访问，false不允许
     public String getHello(){
         return "hello";
     }
