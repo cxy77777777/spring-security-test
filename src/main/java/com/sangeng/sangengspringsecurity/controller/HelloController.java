d10fc49fbb7ffa23a6440dea5ee24d55b1614785
fuyunhao <fh6584207>
2022-06-08 17:31:46 +0800

    开启鉴权功能

--- src/main/java/com/sangeng/sangengspringsecurity/controller/HelloController.java
+++ src/main/java/com/sangeng/sangengspringsecurity/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.sangeng.sangengspringsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("getHello")
+    @PreAuthorize("hasAuthority('test')")
     public String getHello(){
         return "hello";
     }
